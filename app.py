182c46f6f12f13fdf0386d29ac8e78f4c5c2da91
visheshkanoongo22 <visheshkanoongo22@gmail.com>
2025-11-01 13:13:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -18,7 +18,6 @@ TIMEZONE = 'Asia/Kolkata'
 GOOGLE_CALENDAR_IMPORT_LINK = 'https://calendar.google.com/calendar/u/0/r/settings/export'
 
 COURSE_DETAILS_MAP = {
-    courses = {
     'AN(A)': {'Faculty': 'Nitin Pillai', 'Venue': 'T6'}, 
     'AN(B)': {'Faculty': 'Nitin Pillai', 'Venue': 'T6'},
     'B2B(A)': {'Faculty': 'Sandip Trada', 'Venue': 'T5'}, 
@@ -62,7 +61,6 @@ COURSE_DETAILS_MAP = {
     'VALU(A)': {'Faculty': 'Dipti Saraf', 'Venue': 'T5'}, 
     'VALU(B)': {'Faculty': 'Dipti Saraf', 'Venue': 'T5'},
     'VALU(D)': {'Faculty': 'Dimple Bhojwani', 'Venue': 'T6'}
- }   
 }
 
 # 3. FUNCTIONS
@@ -500,6 +498,7 @@ if not master_schedule_df.empty and student_data_map:
             st.error(f"Roll Number '{roll_number}' not found. Please check the number and try again.")
 else:
     st.warning("Application is initializing or required data files are missing. Please wait or check the folder.")
+
 
 
 
