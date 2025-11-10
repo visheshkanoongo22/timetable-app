72d72e5311c89a3d7521960a088cc4cff1d59e83
visheshkanoongo22 <visheshkanoongo22@gmail.com>
2025-11-11 01:37:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -477,12 +477,13 @@ if not master_schedule_df.empty and student_data_map:
 
                 # --- SEARCH BAR (using st_keyup) ---
                 search_query = st_keyup(
-                    " ", # <-- Set label to an empty space
-                    placeholder="e.g., DRM, Himanshu Chauhan, T5, etc", # <-- Placeholder changed
-                    debounce=100, 
+                    "Search by any Subject/Faculty/Classroom:", # <-- Text is now the label
+                    placeholder="e.g., DRM, SMKT, LSS, etc", 
+                    debounce=300, 
                     key=f"search_bar_{st.session_state.search_clear_counter}" 
                 )
-                st.caption("Search by any Subject/Faculty/Classroom") # <-- Label moved to caption below
+                # The st.caption() line has been removed
+                st.caption("Search any subject") # <-- Label moved to caption below
                 search_query = search_query.lower() if search_query else ""
                 
                 # --- CLEAR SEARCH BUTTON ---
