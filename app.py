ccbc6656c285a7b04546bfb899bd946862bea340
visheshkanoongo22 <visheshkanoongo22@gmail.com>
2025-12-07 23:41:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -459,6 +459,174 @@ MESS_MENU = {
         * Gulab jamun
         """
     },
+# Monday (08/12/2025)
+    date(2025, 12, 8): {
+        "Breakfast": \"\"\"
+        * Indori Poha, Lemon Onion, Sev
+        * Fruit + Mix Sprout + Veg Sandwich
+        \"\"\",
+        "Lunch": \"\"\"
+        * Gajar Methi Mutter
+        * Kadi Pakoda
+        * Dal Fry & Plain Rice
+        * Roti
+        * Green Salad, Onion + Lemon
+        * Lemon Water
+        \"\"\",
+        "Hi-Tea": \"\"\"
+        * Tea / Coffee
+        \"\"\",
+        "Dinner": \"\"\"
+        * Cheese Kofta Masala
+        * Aloo Chips Masala
+        * Dal Fry & Jeera Rice
+        * Roti
+        * Onion + Lemon
+        * Frymes
+        \"\"\"
+    },
+    
+    # Tuesday (09/12/2025)
+    date(2025, 12, 9): {
+        "Breakfast": \"\"\"
+        * Sandwich Dhokla with Green Chutney
+        * Fruit + Methi ka Thepla with Curd & Pickle
+        \"\"\",
+        "Lunch": \"\"\"
+        * Paneer Handi Masala
+        * Loki Chana Dal
+        * Dal Fry & Plain Rice
+        * Roti
+        * Chana Chaat Salad
+        * Onion + Lemon
+        * Butter Milk
+        \"\"\",
+        "Hi-Tea": \"\"\"
+        * Tea / Coffee
+        \"\"\",
+        "Dinner": \"\"\"
+        * **Sweet: Jalebi**
+        * Pav Bhaji & Veg Pulav
+        * Boondi Raita
+        * Manchurian Noodles
+        * Sev Roll with Chutney
+        * Roasted Papad
+        * Onion + Lemon
+        \"\"\"
+    },
+    
+    # Wednesday (10/12/2025)
+    date(2025, 12, 10): {
+        "Breakfast": \"\"\"
+        * Masala Mix Uttapam, Sambhar, Chutney
+        * Fruit Poha Sambhar + Toast
+        \"\"\",
+        "Lunch": \"\"\"
+        * Veg Makhanwala
+        * Kala Chana Dry
+        * Dal Mix Tadka & Jeera Rice
+        * Roti
+        * Green Salad, Onion Lemon
+        * Plain Curd
+        \"\"\",
+        "Hi-Tea": \"\"\"
+        * Tea / Coffee
+        \"\"\",
+        "Dinner": \"\"\"
+        * **Soup: Cheese Corn**
+        * Chinese Samosa
+        * Aloo Mutter Tamatar
+        * Plain Kadi & Rajwadi Khichdi
+        * Lasan ki Chatni
+        * Methi Thepla & Roti
+        * **Sweet: Pineapple Halwa**
+        \"\"\"
+    },
+    
+    # Thursday (11/12/2025)
+    date(2025, 12, 11): {
+        "Breakfast": \"\"\"
+        * Sev Khamani with Green Chutney
+        * Fruit Bread Pakoda + Khakhra + Bhakhri
+        \"\"\",
+        "Lunch": \"\"\"
+        * Palak Corn Capsicum
+        * Rajma Masala
+        * Dal Tadka & Mutter Rice
+        * Roti
+        * Green Salad, Onion Lemon
+        * Butter Milk
+        \"\"\",
+        "Hi-Tea": \"\"\"
+        * Tea / Coffee
+        \"\"\",
+        "Dinner": \"\"\"
+        * Gobi Mutter Tamatar
+        * Moong Masala
+        * Hot Pot Rice
+        * Palak Paneer Paratha
+        * Masala Papad, Onion Lemon
+        * Plain Curd
+        * Roti
+        \"\"\"
+    },
+    
+    # Friday (12/12/2025)
+    date(2025, 12, 12): {
+        "Breakfast": \"\"\"
+        * Dabeli with Chutney
+        * Fruit + Masala Maggi + Corn Flakes
+        \"\"\",
+        "Lunch": \"\"\"
+        * Paneer Bhurji Masala
+        * Jeera Aloo Masala
+        * Dal Fry & Plain Rice
+        * Roti
+        * Green Salad, Onion Lemon
+        * Lemon Water
+        \"\"\",
+        "Hi-Tea": \"\"\"
+        * Tea / Coffee
+        \"\"\",
+        "Dinner": \"\"\"
+        * **Dal Batti Rajasthani**
+        * **Sweet: Churma**
+        * Besan ka Gatta Masala
+        * Lasan ki Chatni, Ghee & Gud
+        * Plain Rice
+        * Onion Lemon, Butter Milk
+        * Roti
+        \"\"\"
+    },
+    
+    # Saturday (13/12/2025)
+    date(2025, 12, 13): {
+        "Breakfast": \"\"\"
+        * Puri Bhaji
+        * Fruit Daliya + Veg Sandwich
+        \"\"\",
+        "Lunch": \"\"\"
+        * Veg Jalfrezi Masala Dry
+        * Paneer Kofta
+        * Dal Mix Tadka & Jeera Rice
+        * Roti
+        * Green Salad, Onion Lemon
+        * Butter Milk
+        \"\"\",
+        "Hi-Tea": \"\"\"
+        * Tea / Coffee
+        \"\"\",
+        "Dinner": \"\"\"
+        * Mix Pakoda
+        * **Sarso ka Saag**
+        * Sev Tamatar
+        * Plain Kadi & Rajwadi Khichdi
+        * Lasan ki Chatni
+        * **Makai ki Roti**
+        * Ghee + Gud
+        * Onion + Lemon, Butter Milk
+        \"\"\"
+    },
 }
 
 # 3. FUNCTIONS
