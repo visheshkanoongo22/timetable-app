8179c4637ee542d982d6dc30494f25ced777fd33
visheshkanoongo22 <visheshkanoongo22@gmail.com>
2026-01-07 17:31:18 +0530

    Update day_overrides.py

--- day_overrides.py
+++ day_overrides.py
@@ -3,5 +3,13 @@ from datetime import date
 DAY_SPECIFIC_OVERRIDES = {
     date(2026, 1, 6): {
         'PS&PS': {'Venue': 'POSTPONED (TBA)'}
+    },
+    date(2026, 1, 8): {
+        'RURMKT(A)': {'Venue': 'E6'},
+        'RURMKT(B)': {'Venue': 'E6'},
+        'IF(A)':     {'Venue': 'E6'},
+        'IF(B)':     {'Venue': 'E6'},
+        'MS(A)':     {'Venue': 'E6'},
+        'GBL':       {'Venue': '308-C'}
     }
 }
