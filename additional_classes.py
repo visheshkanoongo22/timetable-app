0c8bfe2975a4196a11a4d1f0db5b149e68c2744b
visheshkanoongo22 <visheshkanoongo22@gmail.com>
2026-01-15 14:45:43 +0530

    Update additional_classes.py

--- additional_classes.py
+++ additional_classes.py
@@ -3,11 +3,4 @@ from datetime import date
 ADDITIONAL_CLASSES = [
     # ... keep any existing classes here ...
 
-    {
-        'Subject': 'IGR&MC',
-        'Date': date(2026, 1, 15),
-        'Time': '6:10-7:10PM',
-        'Venue': 'E1',      # From your previous instruction
-        'Faculty': 'Somayya Madakam'
-    },
 ]
