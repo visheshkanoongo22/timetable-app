5e6d713395b47e5e7a952290360d63bc490ae5f7
visheshkanoongo22 <visheshkanoongo22@gmail.com>
2026-01-19 12:08:23 +0530

    Update additional_classes.py

--- additional_classes.py
+++ additional_classes.py
@@ -1,13 +1,19 @@
 from datetime import date
 
 ADDITIONAL_CLASSES = [
-    # ... keep any existing classes here ...
     {
         "Subject": "D&IT",
         "Date": date(2026, 1, 22),
-        "Time": "06:00-08:20PM",
-        "Venue": "-",
-        "Faculty": "-" 
-    }
+        "Time": "06:10-07:10PM",
+        "Venue": "T6", # Or your designated venue
+        "Faculty": "Dhaval Patanvadia"
+    },
+    {
+        "Subject": "D&IT",
+        "Date": date(2026, 1, 22),
+        "Time": "07:20-08:20PM",
+        "Venue": "T6", # Or your designated venue
+        "Faculty": "Dhaval Patanvadia"
+    },
 
 ]
