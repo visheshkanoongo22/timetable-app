ec169e98ed7e20460092bbe46838899e849e359f
visheshkanoongo22 <visheshkanoongo22@gmail.com>
2026-02-04 16:38:39 +0530

    Update day_overrides.py

--- day_overrides.py
+++ day_overrides.py
@@ -111,7 +111,11 @@ DAY_SPECIFIC_OVERRIDES = {
 
     date(2026, 2, 5): {
         "RURMKT(A)": {"Venue": "T1"},
-        "RURMKT(B)": {"Venue": "T3", "Time": '8-9AM'}
+        "RURMKT(B)": {"Venue": "T3", "Time": '8-9AM'},
+        "PPC(A)": {"Venue": "T6"},
+        "PPC(B)": {"Venue": "T6"},
+        "IF(A)":  {"Venue": "T3"},
+        "IGR&MC": {"Venue": "E3"}
     },
 
 
