de1a46cd50628c02b09bc5020290703e91ac07cd
visheshkanoongo22 <visheshkanoongo22@gmail.com>
2026-02-15 20:25:00 +0530

    Update day_overrides.py

--- day_overrides.py
+++ day_overrides.py
@@ -54,7 +54,7 @@ DAY_SPECIFIC_OVERRIDES = {
     date(2026, 1, 28): {
         "PS&PS": {"Venue": "POSTPONED"},
         "IL(A)": {"Venue": "POSTPONED"},
-        "IL(B)": {"Venue": "POSTPONED"},
+        "IL(B)": {"Venue": "POSTPONED"}
     },
 
    date(2026, 1, 29): {
